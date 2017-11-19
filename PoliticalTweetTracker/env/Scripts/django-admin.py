5cd2a35d1a890388773899f12e5757e4d7479b6d
Kyle Pasco <kylepasco@gmail.com>
2017-11-18 19:40:15 -0800

    Create django project

---
+++ PoliticalTweetTracker/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kylep\source\repos\PoliticalTweetTracker\PoliticalTweetTracker\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
